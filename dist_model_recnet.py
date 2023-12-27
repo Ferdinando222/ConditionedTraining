f3996c2d856dc77280e44edb3589bab6ed2c12c3
Ferdinando222 <71137846+Ferdinando222@users.noreply.github.com>
2023-12-27 18:08:40 +0100

    data

--- dist_model_recnet.py
+++ dist_model_recnet.py
@@ -101,12 +101,12 @@ def main(args):
 
     print(np_data)
 
-    dataset.load_file(os.path.join('train', args.file_name+'-input.wav'), 'train')
+    dataset.load_file(os.path.join('/kaggle/working/ConditionedTraining/Data/train', args.file_name), 'train')
 
 
     dataset.create_subset('val')
     print(os.path.join('val', args.file_name))
-    dataset.load_file(os.path.join('val', args.file_name), 'val')
+    dataset.load_file(os.path.join('/kaggle/working/ConditionedTraining/Data/val', args.file_name), 'val')
 
         # If training is restarting, this will ensure the previously elapsed training time is added to the total
     init_time = time.time() - start_time + train_track['total_time']*3600
